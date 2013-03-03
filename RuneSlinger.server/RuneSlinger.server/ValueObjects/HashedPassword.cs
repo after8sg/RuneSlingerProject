0f90ef70bb632078afd13f79ee12b273b48e81a1
teokk <after8sg@yahoo.com.sg>
2013-03-03 14:22:09 +0800

    commit again

---
+++ RuneSlinger.server/RuneSlinger.server/ValueObjects/HashedPassword.cs
@@ -0,0 +1,22 @@
+﻿namespace RuneSlinger.server.ValueObjects
+{
+    public class HashedPassword
+    {
+        public string Hash { get; private set; }
+        public string Salt { get; private set; }
+
+        public HashedPassword(string hash, string salt)
+        {
+            Hash = hash;
+            Salt = salt;
+        }
+
+        /// <summary>
+        /// for nhibernate
+        /// </summary>
+        private HashedPassword()
+        {
+        }
+
+    }
+}
